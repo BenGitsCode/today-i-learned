dc66393ad15f4bdd4b1139a75db7920b8d4e7016
Ben Adamski <benjadamski@gmail.com>
2017-08-11 11:50:06 -0400

    Add TODO comment
    Don't have time atm but I'd like to keep building on this.

--- til-gen.sh
+++ til-gen.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+
 TIL=my-new-til.md
+
 if [ $# -gt 0 ]; then
   TIL=$1
 fi
@@ -8,3 +10,4 @@ fi
 cat til-template.md > "${TIL}"
 
 
+# TODO make it add markdown file extension by default so that the command syntax could be `til-gen.sh <filename>` and it will append .md no matter what. Currently you have to sepcify `til-gen.sh <filename.md>`
